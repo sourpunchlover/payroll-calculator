37c6d6d419caf857718917c48edf7c81a3714abd
sourpunchlover <sndgpdesigns@gmail.com>
2026-04-08 15:18:04 -0400

    Initial commit

--- src/main/java/com/pluralsight/PayrollApp.java
+++ src/main/java/com/pluralsight/PayrollApp.java
@@ -1,4 +1,11 @@
 package com.pluralsight;
 
+import java.util.Scanner;
+
 public class PayrollApp {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+
+    }
 }
