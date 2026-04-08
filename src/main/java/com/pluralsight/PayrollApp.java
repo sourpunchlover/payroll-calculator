e8d6b656dc81b24caa5f9156851ae0ce56afbf11
sourpunchlover <sndgpdesigns@gmail.com>
2026-04-08 15:51:40 -0400

    Initial commit

--- src/main/java/com/pluralsight/PayrollApp.java
+++ src/main/java/com/pluralsight/PayrollApp.java
@@ -1,10 +1,29 @@
 package com.pluralsight;
 
+import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class PayrollApp {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter name:");
+        String your_name = sc.nextLine();
+
+        System.out.print("Hours worked:");
+        float hours_worked = sc.nextFloat();
+        System.out.print("Pay rate:");
+        float pay_rate = sc.nextFloat();
+        sc.nextLine();
+
+        float total = hours_worked*pay_rate;
+
+        System.out.printf("%f * %f = %f", hours_worked, pay_rate, total );
+
+
+
+
+
 
 
     }
